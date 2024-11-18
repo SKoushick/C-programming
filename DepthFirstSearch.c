0985caf7c8740fa6fe7af4b7257fc6e05cb073f5
SKoushick <86823538+SKoushick@users.noreply.github.com>
2024-11-18 19:12:35 +0530

    Update DepthFirstSearch.c

---
+++ DepthFirstSearch.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void DFS(int);
+int G[10][10],visited[10],n; //n is no of vertices anc
+void main
+int 1,j;
+printf("Enter number of vertices:");
+scanf("d", &n);
+/read the adjecency matrix
+printf("nEnter adjecency matrix of the graph:");
+for(i=0;i<n;i++)
+forf=0;j<n;++)
+scanf("d",&G[]0]);
+//visited is initialized to zero
+for(i=0;i<n;i++)
+visited[i]=0;
+DFS(0);
+}
+void DFS(int i)
+{
+int j;
+printf(" n%d",i);
+visited [i]=1;
+for(j=0;j<n;j++)
+if!visitedf]&&G(10|==1)
+DFS(j);}
